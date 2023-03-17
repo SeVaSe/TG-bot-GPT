7da1059d2c06df1714e06991d7b65c0a24d8bd63
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-03-17 16:04:35 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -2,8 +2,8 @@ import openai
 from aiogram.dispatcher import Dispatcher
 from aiogram import Bot, types
 
-token = '6077269183:AAFSR4HlkvoS7fjl4Y7CkPdtKdh5G75LABU'
-openai.api_key = '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'
+token = 'Ваш токен бота'
+openai.api_key = 'Ваш токен OpenAI бота'
 
 bot = Bot(token)
 disp = Dispatcher(bot)
